f505cd45c37fe9df5dbcc21b700f66cc1d18eab1
Juan Pablo Ruiz <juan.039@gmail.com>
2023-02-01 20:26:47 -0500

    Add LogInView component

---
+++ src/views/LogInView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about">
+    <h1>Welcome to your Pokedex</h1>
+
+    <div class="container">
+      <LogIn />
+    </div>
+  </div>
+</template>
+
+<script>
+import LogIn from '@/components/LogIn.vue'
+
+export default {
+  name: 'LogInView',
+  components: {
+    LogIn
+  }
+}
+</script>
