a9f04cada6a3b5806e3c591521d99f087c177b0c
Juan Pablo Ruiz <juan.039@gmail.com>
2023-02-01 20:27:09 -0500

    Add logic to the Login and then redirect

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import LogInView from '../views/LogInView.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: LogInView
   },
   {
     path: '/about',
@@ -22,6 +28,26 @@ const routes = [
 
 const router = new VueRouter({
   routes
+})
+
+const isLoggedIn = () => {
+  return localStorage.getItem('token')
+}
+
+router.beforeEach((to, from, next) => {
+  if(!isLoggedIn() && to.name !== 'login'){
+      next({
+          path: '/login'
+      })
+  }else next()
+})
+
+router.beforeEach((to, from, next) => {
+  if(isLoggedIn() && to.name == 'login'){
+      next({
+          path: '/'
+      })
+  }else next()
 })
 
 export default router
